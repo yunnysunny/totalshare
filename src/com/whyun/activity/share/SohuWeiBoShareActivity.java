6e71e43a10bedb6750ae2cbf4f9c0763fa8d8af4
yunnysunny <yunnysunny@gmail.com>
2012-04-23 21:29:18 +0800

    删除掉对搜狐微博的activity引用

--- src/com/whyun/activity/share/SohuWeiBoShareActivity.java
+++ src/com/whyun/activity/share/SohuWeiBoShareActivity.java
@@ -2,6 +2,10 @@ package com.whyun.activity.share;
 
 import com.whyun.activity.share.bean.ShareUrl;
 
+/**
+ * The Class SohuWeiBoShareActivity.
+ * 中文显示乱码
+ */
 public class SohuWeiBoShareActivity extends AbstractShareActivity {
 	private static final ShareUrl SHARE_URL 
 	= ShareUrlFactory.getInstance(IShare.SOHU_WEI_BO);
